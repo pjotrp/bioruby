32138a954ad890e537582b472ed43aca62660b78
Pjotr <pjotr.public05@thebird.nl>
2008-08-22 10:09:34 +0200

    Read external data files

--- lib/bio/db/microarray/miniml/miniml.rb
+++ lib/bio/db/microarray/miniml/miniml.rb
@@ -26,6 +26,62 @@ module Bio
 
       module XML
       end # XML
+
+      # Definitition of a GEO family file - describing a series of microarrays
+      class GEO_Family
+
+        # Load the MINiML family file - assuming all related files are stored
+        # in the same path
+        def initialize xmlfn
+          @path = File.dirname(xmlfn)
+          @fn   = File.basename(xmlfn)
+          $stderr.print "Loading #{@fn} from #{@path}\n" if $VERBOSE 
+          @xml = XmlSimple.xml_in(xmlfn, { 'KeyAttr' => 'name' })
+        end
+
+        # Returns a Hash of sample information
+        def each_sample
+          @xml['Sample'].each do | sample |
+            yield sample
+          end
+        end
+
+        # Iterate sample accessions - returns GEO accession 
+        def each_sample_acc
+          each_sample do | sample |
+            yield sample['iid']
+          end
+        end
+
+        # Fetch the data points for sample +acc+ using +options+. Returns
+        # data points as an array (by default ID and VALUE).
+        #
+        def each_row acc, options = {:columns=>["ID_REF", "VALUE"]}
+          names = options[:columns]
+          # ---- get the sample layout
+          each_sample do | sample |
+            if sample['iid'] == acc
+              datafn = sample['Data-Table'][0]['External-Data'][0]['content'].strip
+              # ---- find the columns
+              positions = []
+              sample['Data-Table'][0]['Column'].each do | column |
+                if names.include? column['Name'][0]
+                  positions.push column['position'].to_i-1
+                end
+              end
+              # ---- read the data file and yield points
+              File.open(@path+'/'+datafn).each_line do | line |
+                fields = line.split(/\t/)
+                yield positions.collect { | pos | fields[pos] }
+              end
+              break
+            end
+          end
+        end
+      end
+
+
+
     end # MINiML
   end # Microarray
 end # Bio
