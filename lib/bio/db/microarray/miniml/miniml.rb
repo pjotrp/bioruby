60636cd80043b5958bee1e77d844dd4c9bcfe72f
Pjotr <pjotr.public05@thebird.nl>
2008-08-22 10:24:43 +0200

    Moved Sample into its own class

--- lib/bio/db/microarray/miniml/miniml.rb
+++ lib/bio/db/microarray/miniml/miniml.rb
@@ -24,12 +24,47 @@ module Bio
 
     module MINiML
 
-      module XML
-      end # XML
+      # Represents a Sample definition (mostly for internal use)
+      class Sample
 
+        # pass a reference to GEO_Family Sample hash
+        def initialize geo_family, sampledata
+          @geo_family = geo_family
+          @data = sampledata
+        end
+
+        # Fetch the data points for sample using +options+. Returns
+        # data points as an array (by default ID and VALUE). A value
+        # column is returned as a Float.
+        #
+        def each_row options = {:columns=>["ID_REF", "VALUE"]}
+          names = options[:columns]
+          value_position = nil
+          # ---- get the sample layout
+          sample = @data
+          datafn = sample['Data-Table'][0]['External-Data'][0]['content'].strip
+          # ---- find the columns
+          positions = []
+          sample['Data-Table'][0]['Column'].each do | column |
+            name = column['Name'][0]
+            if names.include? column['Name'][0]
+              value_position = positions.size if name == 'VALUE'
+              positions.push column['position'].to_i-1
+            end
+          end
+          # ---- read the data file and yield points
+          File.open(@geo_family.path+'/'+datafn).each_line do | line |
+            fields = line.split(/\t/)
+            yield positions.collect { | pos | ( pos==value_position ? fields[pos].to_f : fields[pos] ) }
+          end
+        end
+
+      end
+  
       # Definitition of a GEO family file - describing a series of microarrays
       class GEO_Family
 
+        attr_reader :path
         # Load the MINiML family file - assuming all related files are stored
         # in the same path
         def initialize xmlfn
@@ -39,10 +74,10 @@ module Bio
           @xml = XmlSimple.xml_in(xmlfn, { 'KeyAttr' => 'name' })
         end
 
-        # Returns a Hash of sample information
+        # Returns a Sample object
         def each_sample
           @xml['Sample'].each do | sample |
-            yield sample
+            yield Sample.new(self,sample)
           end
         end
 
@@ -53,31 +88,6 @@ module Bio
           end
         end
 
-        # Fetch the data points for sample +acc+ using +options+. Returns
-        # data points as an array (by default ID and VALUE).
-        #
-        def each_row acc, options = {:columns=>["ID_REF", "VALUE"]}
-          names = options[:columns]
-          # ---- get the sample layout
-          each_sample do | sample |
-            if sample['iid'] == acc
-              datafn = sample['Data-Table'][0]['External-Data'][0]['content'].strip
-              # ---- find the columns
-              positions = []
-              sample['Data-Table'][0]['Column'].each do | column |
-                if names.include? column['Name'][0]
-                  positions.push column['position'].to_i-1
-                end
-              end
-              # ---- read the data file and yield points
-              File.open(@path+'/'+datafn).each_line do | line |
-                fields = line.split(/\t/)
-                yield positions.collect { | pos | fields[pos] }
-              end
-              break
-            end
-          end
-        end
       end
 
 
